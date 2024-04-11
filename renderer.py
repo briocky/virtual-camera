620185bb99550f0cc4daf6d3f94d7724d4885eb8
dobos <doboszdav@gmail.com>
2024-04-12 01:52:47 +0200

    virtual camera

--- renderer.py
+++ renderer.py
@@ -4,15 +4,17 @@ import pygame
 
 
 def draw_scene(camera, screen, vertices, edges):
+    # project points to 2D
     projected_points = []
     for point in vertices:
         projected_point = project_3d_to_2d(point, camera, SCREEN_WIDTH, SCREEN_HEIGHT, FOV)
         projected_points.append(projected_point)
         if projected_point:
             pygame.draw.circle(screen, WHITE, projected_point, 5)
+
+    # draw connection lines
     for edge in edges:
         start = projected_points[edge[0] - 1]
         end = projected_points[edge[1] - 1]
         if start and end:
-        # print(start, end)
             pygame.draw.line(screen, WHITE, start[0:2], end[0:2], 2)
