5956f063339891cb7794ba2c51475c414d90e645
dobos <doboszdav@gmail.com>
2024-04-12 02:03:17 +0200

    virtual camera

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 class Camera:
-    def __init__(self, pos, yaw, pitch, roll, move_delta=0.2, rot_delta=5):
+    def __init__(self, pos, yaw, pitch, roll, move_delta=10, rot_delta=0.1):
         self.pos = pos
         self.yaw = yaw
         self.pitch = pitch
@@ -8,37 +8,37 @@ class Camera:
         self.rot_delta = rot_delta
 
     def move_forward(self):
-        self.pos[2] += 10
+        self.pos[2] += self.move_delta
 
     def move_backward(self):
-        self.pos[2] -= 10
+        self.pos[2] -= self.move_delta
 
     def move_left(self):
-        self.pos[0] -= 10
+        self.pos[0] -= self.move_delta
 
     def move_right(self):
-        self.pos[0] += 10
+        self.pos[0] += self.move_delta
 
     def move_up(self):
-        self.pos[1] += 10
+        self.pos[1] += self.move_delta
 
     def move_down(self):
-        self.pos[1] -= 10
+        self.pos[1] -= self.move_delta
 
     def rotate_up(self):
-        self.pitch += 0.1
+        self.pitch += self.rot_delta
 
     def rotate_down(self):
-        self.pitch -= 0.1
+        self.pitch -= self.rot_delta
 
     def rotate_left(self):
-        self.yaw += 0.1
+        self.yaw += self.rot_delta
 
     def rotate_right(self):
-        self.yaw -= 0.1
+        self.yaw -= self.rot_delta
 
     def roll_right(self):
-        self.roll -= 0.1
+        self.roll -= self.rot_delta
 
     def roll_left(self):
-        self.roll += 0.1
+        self.roll += self.rot_delta
